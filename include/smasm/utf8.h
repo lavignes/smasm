20677772f4fcdbfb6ce6c82d5d8098a1c415c79e
Scott LaVigne <lavignes@amazon.com>
2025-04-03 00:13:14 -0700

    utf8

--- include/smasm/utf8.h
+++ include/smasm/utf8.h
@@ -5,6 +5,8 @@
 
 U32 smUtf8Decode(U8 const buf[4], UInt *len);
 
-UInt smUtf8Encode(U32 codepoint, U8 buf[4]);
+UInt smUtf8Encode(U32 c, U8 buf[4]);
+
+UInt smUtf8Len(SmBuf buf);
 
 #endif // SMASM_UTF8_H
--- src/libsmasm/utf8.c
+++ src/libsmasm/utf8.c
@@ -1,40 +1,80 @@
 #include <smasm/utf8.h>
 
-#include <stdlib.h>
-
 U32 smUtf8Decode(U8 const buf[4], UInt *len) {
-    if ((buf[0] & 0x80) == 0x00) {
-        if (len != NULL) {
-            *len = 1;
-        }
+    if (buf[0] < 0x80) {
+        *len = 1;
         return buf[0];
     }
-    if (((buf[0] & 0xE0) == 0xC0) && ((buf[1] & 0xC0) == 0x80)) {
-        if (len != NULL) {
-            *len = 2;
-        }
-        return (((U32)(buf[0] & 0x1F)) << 6) | (buf[1] & 0x3F);
+    if ((buf[0] & 0xE0) == 0xC0) {
+        U32 c = buf[0] & 0x1F;
+        c     = (c << 6) | (buf[1] & 0x3F);
+        *len  = 2;
+        return c;
     }
-    if (((buf[0] & 0xF0) == 0xE0) && ((buf[1] & 0xC0) == 0x80) &&
-        ((buf[2] & 0xC0) == 0x80)) {
-        if (len != NULL) {
-            *len = 3;
-        }
-        return (((U32)(buf[0] & 0x0F)) << 12) | (((U32)(buf[1] & 0x3F)) << 6) |
-               (buf[2] & 0x3F);
+    if ((buf[0] & 0xF0) == 0xE0) {
+        U32 c = buf[0] & 0x0F;
+        c     = (c << 6) | (buf[1] & 0x3F);
+        c     = (c << 6) | (buf[2] & 0x3F);
+        *len  = 3;
+        return c;
     }
-    if (((buf[0] & 0xF8) == 0xF0) && ((buf[1] & 0xC0) == 0x80) &&
-        ((buf[2] & 0xC0) == 0x80) && ((buf[3] & 0xC0) == 0x80)) {
-        if (len != NULL) {
-            *len = 4;
-        }
-        return (((U32)(buf[0] & 0x0F)) << 18) | (((U32)(buf[1] & 0x3F)) << 12) |
-               (((U32)(buf[2] & 0x3F)) << 6) | ((U32)(buf[3] & 0x3F));
+    if ((buf[0] & 0xF8) == 0xF0) {
+        U32 c = buf[0] & 0x07;
+        c     = (c << 6) | (buf[1] & 0x3F);
+        c     = (c << 6) | (buf[2] & 0x3F);
+        c     = (c << 6) | (buf[3] & 0x3F);
+        *len  = 4;
+        return c;
     }
-    if (len != NULL) {
-        *len = 0;
-    }
+    *len = 0;
     return 0xFFFD;
 }
 
-UInt smUtf8Encode(U32 codepoint, U8 buf[4]);
+UInt smUtf8Encode(U32 c, U8 buf[4]) {
+    if (c < 0x7F) {
+        buf[0] = c;
+        return 1;
+    }
+    if (c <= 0x7FF) {
+        buf[0] = 0xC0 | (c >> 6);
+        buf[1] = 0x80 | (c & 0x3F);
+        return 2;
+    }
+    if (c <= 0xFFFF) {
+        buf[0] = 0xE0 | (c >> 12);
+        buf[1] = 0x80 | ((c >> 6) & 0x3F);
+        buf[2] = 0x80 | (c & 0x3F);
+        return 3;
+    }
+    if (c <= 0x10FFFF) {
+        buf[0] = 0xF0 | (c >> 18);
+        buf[1] = 0x80 | ((c >> 12) & 0x3F);
+        buf[2] = 0x80 | ((c >> 6) & 0x3F);
+        buf[3] = 0x80 | (c & 0x3F);
+        return 4;
+    }
+    return 0;
+}
+
+UInt smUtf8Len(SmBuf buf) {
+    UInt len = 0;
+    for (UInt i = 0; i < buf.len; ++len) {
+        if (buf.bytes[i] < 0x80) {
+            ++i;
+            continue;
+        }
+        if ((buf.bytes[i] & 0xE0) == 0xC0) {
+            i += 2;
+            continue;
+        }
+        if ((buf.bytes[i] & 0xF0) == 0xE0) {
+            i += 3;
+            continue;
+        }
+        if ((buf.bytes[i] & 0xF8) == 0xF0) {
+            i += 4;
+            continue;
+        }
+    }
+    return len;
+}
